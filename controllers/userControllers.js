eaa9587adc87059dd3334ee2459b90956148fa82
Viswak23 <Viswak.RameshBalakrishnan@metropolia.fi>
2024-09-22 20:22:33 +0530

    iteration 7 completed

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -41,7 +41,7 @@ const getUserById = async (req, res) => {
 };
 
 // PUT /users/:userId
-const updateUser = async (req, res) => {
+/* const updateUser = async (req, res) => {
   const userId = req.params.userId;
   if (!mongoose.Types.ObjectId.isValid(userId)) {
     return res.status(400).json({ message: "Invalid user ID" });
@@ -56,6 +56,31 @@ const updateUser = async (req, res) => {
       res.status(200).json(user);
     } else {
       res.status(404).json({ message: "user not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Failed to update user" });
+  }
+}; */
+
+//patch
+
+const updateUser = async (req, res) => {
+  const { userId } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(userId)) {
+    return res.status(400).json({ message: "Invalid User ID" });
+  }
+
+  try {
+    const updatedUser = await User.findOneAndReplace(
+      { _id: userId },
+      { ...req.body },
+      { new: true }
+    );
+    if (updatedUser) {
+      res.status(200).json(updatedUser);
+    } else {
+      res.status(404).json({ message: "User not found" });
     }
   } catch (error) {
     res.status(500).json({ message: "Failed to update user" });

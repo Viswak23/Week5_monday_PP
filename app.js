19d7c4aaa45f9633d332999f274b14c02d800e55
Viswak23 <Viswak.RameshBalakrishnan@metropolia.fi>
2024-09-22 19:58:10 +0530

    added .env

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const { unknownEndpoint } = require("./middleware/customMiddleware");
 const connectDB = require("./config/db");
 
 const morgan = require("morgan");
+require("dotenv").config();
+const port = process.env.PORT || 4000;
+
 app.use(morgan("dev"));
 
 // Middleware to parse JSON
@@ -20,7 +23,7 @@ app.use("/api/users", userRouter);
 app.use(unknownEndpoint);
 // app.use(errorHandler);
 connectDB();
-const port = process.env.PORT || 4000;
+
 // Start the server
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
